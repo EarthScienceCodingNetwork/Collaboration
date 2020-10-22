ef8355736173315d7e8d69ce353628b0501ade22
Andrew <armurray@live.unc.edu>
2020-10-22 11:16:50 -0400

    Added bw theme

--- figure1.R
+++ figure1.R
@@ -2,5 +2,6 @@ library(tidyverse)
 
 ggplot(mtcars)+
   geom_point(aes(x=hp,y=wt, color = as.factor(gear)))+
-               scale_color_manual(values = c("#a42bbd","#2dc23e","#eb4034"),name = "Gears")
+               scale_color_manual(values = c("#a42bbd","#2dc23e","#eb4034"),name = "Gears")+
+  theme_bw()
 
