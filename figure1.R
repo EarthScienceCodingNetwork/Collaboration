ee10c833d25c9e489c1eced8ee800939bd17facf
Natalie Schmer <schmer.natalie@gmail.com>
2020-10-22 09:26:41 -0600

    added a facet wrap

--- figure1.R
+++ figure1.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 
 ggplot(mtcars)+
-  geom_point(aes(x=hp,y=wt, color = as.factor(gear)))+
+  geom_point(aes(x=hp,y=wt))+
                scale_color_manual(values = c("#a42bbd","#2dc23e","#eb4034"),name = "Gears")+
-  theme_bw()
+  theme_bw()+
+    facet_wrap(~ gear)
 
