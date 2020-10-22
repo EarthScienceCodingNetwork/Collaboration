b2172e810e2ecd3275c83d15a80c907093035551
Chamberlin <Chamberlin.Catherine@epa.gov>
2020-10-22 11:26:26 -0400

    Switched the Y-axis to log10. Purpose: different visualization

--- figure1.R
+++ figure1.R
@@ -2,7 +2,11 @@ library(tidyverse)
 
 ggplot(mtcars)+
   geom_point(aes(x=hp,y=wt, color = as.factor(gear)))+
-               scale_color_manual(values = c("#a42bbd","#2dc23e","#eb4034"),name = "Gears")+
+               scale_color_manual(
+                 values = c("#a42bbd","#2dc23e","#eb4034"),
+                 name = "Gears"
+                 )+
   labs(c = "X",y = "Y")+
-  theme_bw()
+  theme_bw() +
+  scale_y_log10()
 
